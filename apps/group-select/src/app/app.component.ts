3112c46d56138a5bd012b90d476178f00e4764cb
Nguyen Duc Manh <manhnd.jin@gmail.com>
2021-03-01 21:08:24 +0700

    initial commit

---
+++ apps/group-select/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'group-select-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'group-select';
+}
